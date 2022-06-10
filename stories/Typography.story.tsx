5d82861c730c09336993c08b4b568158f80ffaaf
yangyuhan.79 <yangyuhan.79@bytedance.com>
2022-06-10 18:46:13 +0800

    feat: 超出省略重构

--- stories/Typography.story.tsx
+++ stories/Typography.story.tsx
@@ -1,40 +1,80 @@
-import React, { useState } from 'react';
-import { Typography, Button } from '@self';
+import React from 'react';
+import { Typography, Table } from '@self';
 
-const defaultText = `A design is a plan or specification for the construction of an object or system or for the
-implementation of an activity or process. A design is a plan or specification for the
-construction of an object or system or for the implementation of an activity or process. A design is a plan or specification for the construction of an object or system or for the
-implementation of an activity or process. A design is a plan or specification for the
-construction of an object or system or for the implementation of an activity or process. `;
+const mockText =
+  'A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or process. The verb to design expresses the process of developing a design. The verb to design expresses the process of developing a design. A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or process. The verb to design expresses the process of developing a design. The verb to design expresses the process of developing a design.';
 
-const DemoExpand = () => {
-  const [expandable, setExpandable] = useState(false);
-  const [expanded, setExpanded] = useState(false);
-  const ellipsisProps = { rows: 1, expandable, onExpand: setExpanded, expanded };
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    render() {
+      return (
+        <Typography.Paragraph
+          ellipsis={{
+            rows: 2,
+          }}
+        >
+          {mockText}
+        </Typography.Paragraph>
+      );
+    },
+  },
+  {
+    title: 'Salary',
+    dataIndex: 'salary',
+    render() {
+      return (
+        <Typography.Paragraph
+          ellipsis={{
+            rows: 2,
+          }}
+        >
+          {mockText}
+        </Typography.Paragraph>
+      );
+    },
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    render() {
+      return (
+        <Typography.Paragraph
+          ellipsis={{
+            rows: 2,
+          }}
+        >
+          {mockText}
+        </Typography.Paragraph>
+      );
+    },
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    render() {
+      return <Typography.Paragraph>email,email</Typography.Paragraph>;
+    },
+  },
+];
 
+function DemoExpand() {
   return (
-    <div>
-      <Button onClick={() => setExpandable(!expandable)}>
-        {!expandable ? '显示按钮' : '不显示按钮'}
-      </Button>
-      <Button onClick={() => setExpanded(!expanded)} style={{ marginLeft: '10px' }} type="primary">
-        {expanded ? '折叠' : '展开'}
-      </Button>
-      <Typography.Text code ellipsis={ellipsisProps}>
-        {defaultText}
-      </Typography.Text>
-      <Typography.Text mark ellipsis={ellipsisProps}>
-        {defaultText}
-      </Typography.Text>
-      <Typography.Text ellipsis={{ rows: 1, cssEllipsis: false, expandable: true }}>
-        {expandable
-          ? 'A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process. A design is a plan or specification for'
-          : defaultText}
-      </Typography.Text>
-    </div>
+    <Table
+      columns={columns}
+      tableLayoutFixed
+      pagination={false}
+      data={new Array(50).fill('').map((_, index) => ({
+        key: index,
+        name: 'Jane Doe',
+        salary: 23000,
+        address: '32 Park Road, London',
+        email: 'jane.doe@example.com',
+      }))}
+    />
   );
-};
-
+}
 export const Expand = () => <DemoExpand />;
 
 export default {
